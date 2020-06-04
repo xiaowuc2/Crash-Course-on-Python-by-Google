56de1c8ed21d76ff4401431186a6973798975da9
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-06-04 19:41:55 +0530

    Update Practice Quiz: Conditionals.py

--- Week 2/Practice Quiz: Conditionals.py
+++ Week 2/Practice Quiz: Conditionals.py
@@ -36,4 +36,24 @@ A file made up of 4097 bytes will use 4096*2=8192 bytes of storage. Knowing this
 can you fill in the gaps in the calculate_storage function below,
 which calculates the total number of bytes needed to store a file of a given size?
 >
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize//block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = block_size%filesize
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return 8192
+    else:
+        return 4096
+        #pass
+        
+    return full_blocks
 
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
